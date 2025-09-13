f21a44139de7b88d9b4fe1a643311840a8f4fa2c
hemachandra Reddy <pottingari@gmailcom>
2025-09-13 19:31:34 +0530

    change in port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config()
 const app = express();
-const PORT = process.env.PORT_ENV || 3000;
+const PORT = process.env.PORT_ENV || 4000;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT} `);
 })
