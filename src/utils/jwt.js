f06e6ebbfef861c6bcd93c355f8d291488e20598
hemachandra Reddy <pottingari@gmailcom>
2025-09-17 23:04:26 +0530

    jwt

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,14 +1,21 @@
 import jwt from 'jsonwebtoken';
 import * as logger from 'morgan';
 
-const JWT_SECRET =
-  process.env.JWT_SECRET || ' THIS IS A SECTRET THAT CANOT BE ISSUED PUBLICLY';
+const JWT_SECRET =process.env.JWT_SECRET || ' THIS IS A SECTRET THAT CANOT BE ISSUED PUBLICLY';
 const JWT_EXPIRY = '1h';
 export const jwttoken = {
-  sign: payload => {
-    try {
-    } catch (e) {
+  sign: (payload)=>{
+    try{
+      return jwt.sign(payload,JWT_SECRET,{expiresIn:JWT_EXPIRY});
+    }catch(e){
       logger.error(e);
     }
   },
+  verify:(token)=>{
+    try{
+      return jwt.verify(token, JWT_SECRET);
+    }catch(e){
+      logger.error(e);
+    }
+  }
 };
